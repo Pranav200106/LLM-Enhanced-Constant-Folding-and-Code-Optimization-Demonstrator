f5e2ee565b5864387fddead4a01181226df83571
Pranav <23z139@psgitech.ac.in>
2025-11-06 22:39:11 +0530

    Updated prompt

--- backend/llm/LLM.py
+++ backend/llm/LLM.py
@@ -340,7 +340,7 @@ def LLM():
     - 2–4 bullet points only
     Keep the total output under 6 lines (suitable for frontend card view).
     Dont use any emojis.
-    Give suggestions with the heading "$Suggestions:$" and enclose each suggestions with "$$".
+    Give suggestions with the heading "$Suggestions:$" and enclose each suggestions like "$$...$$" compulsorily.
     Also compulsorily generate your version of the optimized three address code with the heading: "$Optimization:$" in the end of the summary.
     All the optimized code must be in newlines. 
     """
